21d9fe2210447a937c7f318cf5284a327537f97f
Peter Bui <peter@Peters-MacBook-Pro.local>
2019-08-14 14:29:21 -0400

    worked on connection to bookings

--- api/routes/saveRoom.js
+++ api/routes/saveRoom.js
@@ -7,21 +7,51 @@ var url = "https://gmu.libcal.com/ajax/space/book";
 
 router.post('/', (req, res) => {
     console.log("reserving room");
+    //console.log(req.body.bookings);
 });
 
 router.get('/', (req, res) => {
-    res.send("save server");
-
-    var form = "";
-    var booking = "";
-
 
     var data = {
-        formData: form,
-        bookings: booking
-    }
+        formData: {
+            fname: 'Peter',
+            lname: 'Bui',
+            email: 'pbui8@masonlive.gmu.edu'
+        },
+        forcedEmail: '',
+        booking: {
+            id: 1,
+            eid: 8102,
+            gid: 2118,
+            lid: 1205,
+            start: '2019-08-14 15:30',
+            end: '2019-08-14 17:30'
+        },
+        returnURL: '/spaces'
+    };
 
-    axios.post(url, data);
+
+    console.log('Sending data...');
+
+    var config = {
+        headers: {
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Referer': 'https://gmu.libcal.com/spaces?lid=1205&gid=2118',
+            'Sec-Fetch-Mode': 'cors',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+
+    } 
+
+    //console.log(data.booking);
+
+    axios.post(url, data, config)
+        .then(info => console.log(info))
+        .catch(error => console.log(error));
+
+    res.send("save server");
 })
 
 module.exports = router;
