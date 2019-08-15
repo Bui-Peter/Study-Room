bb63571b4b04f5b3aa68ebaa73a14232582a7e05
Peter Bui <peter@peters-mbp.lan>
2019-08-14 21:25:24 -0400

    add map of lid to room number

--- api/routes/saveRoom.js
+++ api/routes/saveRoom.js
@@ -5,29 +5,53 @@ var axios = require('axios');
 
 var url = "https://gmu.libcal.com/ajax/space/book";
 
+var roomCoord = new Map();
+
+// lobby assignments
+for(var i = 0; i <= 5; i++) roomCoord.set(1201 + i, 8200 + i);
+
+// 2
+roomCoord.set(1012, 8060);
+roomCoord.set(1707, 8063);
+roomCoord.set(3606, 8067);
+roomCoord.set(3608, 8068);
+roomCoord.set(3609, 8069);
+roomCoord.set(3610, 8070);
+roomCoord.set(3702, 8071);
+roomCoord.set(3706, 8072);
+roomCoord.set(3708, 8073);
+roomCoord.set(4612, 8074);
+roomCoord.set(4613, 8075);
+roomCoord.set(4614, 8076);
+roomCoord.set(4702, 8077);
+roomCoord.set(4706, 8078);
+roomCoord.set(4708, 8079);
+
+roomCoord.set(3002, 8102);
+roomCoord.set(3605, 8104);
+roomCoord.set(3705, 8106);
+roomCoord.set(3713, 8107);
+roomCoord.set(4003, 8103);
+roomCoord.set(4603, 8108);
+roomCoord.set(4701, 8109);
+roomCoord.set(4705, 8110);
+roomCoord.set(4713, 8111);
+
+roomCoord.set(3101, 8112);
+roomCoord.set(3103, 8113);
+roomCoord.set(4103, 8114);
+roomCoord.set(4104, 8115);
+roomCoord.set(4106, 8116);
+
+roomCoord.set(8117, 3102);
+roomCoord.set(8118, 3111);
+
 router.post('/', (req, res) => {
     console.log("reserving room");
     //console.log(req.body.bookings);
 });
 
 router.get('/', (req, res) => {
-
-    var data = {
-        formData: {
-            fname: 'Peter',
-            lname: 'Bui',
-            email: 'pbui8@masonlive.gmu.edu'
-        },
-        booking: {
-            id: 1,
-            eid: 8102,
-            gid: 2118,
-            lid: 1205,
-            start: '2019-08-15 15:30',
-            end: '2019-08-15 17:30'
-        },
-    };
-
 
     console.log('Sending data...');
 
@@ -47,13 +71,26 @@ router.get('/', (req, res) => {
 
     //eid
     // 8200 - 8204 = 1201 - 1205 (lobby)
-    // 8060, 8063, 8067, 8068, 8069, 8070, 8071, 8072, 8073, 8074, 8075, 8076, 8077, 8079, 8079 = 1012, 1707, 3606, 3608, 3609, 3610, 3702, 3706, 3708, 4612, 4613, 4614, 4702, 4706, 4708 (room for 2)
+    // 8060, 8063, 8067, 8068, 8069, 8070, 8071, 8072, 8073, 8074, 8075, 8076, 8077, 8078, 8079 = 1012, 1707, 3606, 3608, 3609, 3610, 3702, 3706, 3708, 4612, 4613, 4614, 4702, 4706, 4708 (room for 2)
     // 8085, 8086, 8087, 8088, 8089. 8090, 8091, 8092, 8093, 8094, 8095, 8100, 8097, 8098, 8099, 8101 = 2004, 3104, 3105, 3106, 3107, 3109, 3110, 3201, 3202, 3401, 3402, 4002, 4009, 4105, 4201, 4202 (4 people room)
     // 8102, 8104, 8106, 8107, 8103, 8108, 8109, 8110, 8111 = 3002, 3605, 3705, 3713, 4003, 4603, 4701, 4705, 4713 (6 people room)
     // 8112 - 8116 = 3101, 3103, 4103, 4104, 4106 (8 people room)
-    // 8117, 8118 = 3102, 3111
+    // 8117, 8118 = 3102, 3111 (presentation room)
 
-    axios.post(url, 'formData%5Bfname%5D=Peter&formData%5Blname%5D=Bui&formData%5Bemail%5D=pbui8%40masonlive.gmu.edu&forcedEmail=&bookings%5B0%5D%5Bid%5D=1&bookings%5B0%5D%5Beid%5D=8200&bookings%5B0%5D%5Bgid%5D=2142&bookings%5B0%5D%5Blid%5D=1205&bookings%5B0%5D%5Bstart%5D=2019-08-15+14%3A00&bookings%5B0%5D%5Bend%5D=2019-08-15+14%3A30&returnUrl=%2Fspaces', config)
+
+    // date format: 2019-08-15+14%3A30
+    var fname = 'Peter';
+    var lname = 'Bui';
+    var email = 'pbui8';
+    var eid = 8079;
+    var gid = 2116;
+    var lid = 1205;
+    var start = '2019-08-15+14%3A30';
+    var end = '2019-08-15+16%3A30';
+
+    var data = 'formData%5Bfname%5D=' + fname + '&formData%5Blname%5D=' + lname + '&formData%5Bemail%5D=' + email + '%40masonlive.gmu.edu&forcedEmail=&bookings%5B0%5D%5Bid%5D=1&bookings%5B0%5D%5Beid%5D=' + eid + '&bookings%5B0%5D%5Bgid%5D=' + gid + '&bookings%5B0%5D%5Blid%5D=' + lid + '&bookings%5B0%5D%5Bstart%5D=' + start + '&bookings%5B0%5D%5Bend%5D=' + end + '&returnUrl=%2Fspaces';
+
+    axios.post(url, data, config)
         .then(info => console.log(info.request))
         .catch(error => console.log(error));
 
